0ef83dbc8137eb252848ef4c45c7de79eb53086c
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-04-12 15:44:29 +0200

    fix duplicate registration number display, add filter by town functionality

--- js/registration_numbers_dom.js
+++ js/registration_numbers_dom.js
@@ -1,22 +1,62 @@
 var regInputElem = document.querySelector(".regNumField");
 var addButtonElem = document.querySelector(".addButton");
-var currentDiv = document.querySelector(".regNumbersDisplay");
+var regNoDispDiv = document.querySelector(".regNumbersDisplay");
+var townSelectElem = document.querySelector(".townSelect");
 
 addButtonElem.addEventListener('click', addButtonClicked);
+townSelectElem.addEventListener('change', handleTownSelectChange);
 
 var addRegNum = AddRegNum();
 
-function addButtonClicked() {
-  var regNum = regInputElem.value;
-  addRegNum.addRegistrationNo(regNum);
+function showRegNumber(regNumber){
+  var parEl = document.createElement("p");
+  var newContent = document.createTextNode(regNumber);
+  parEl.appendChild(newContent);
+  regNoDispDiv.appendChild(parEl);
+}
 
-  var regNumList = addRegNum.getRegistrationNos();
-  console.log( regNumList)
-  Object.keys(regNumList).map(regNum =>{
-    var parEl = document.createElement("p");
-    var newContent = document.createTextNode(regNum);
-    parEl.appendChild(newContent);
-    currentDiv.appendChild(parEl);
+function clearRegNumbers(){
+  var parEl = document.querySelector("p");
+
+
+  while (regNoDispDiv.firstChild) {
+    regNoDispDiv.removeChild(regNoDispDiv.firstChild);
+}
+
+}
+
+function addButtonClicked() {
+    var regNum = regInputElem.value;
+    addRegNum.addRegistrationNo(regNum);
+    showRegNumber(regNum);
+}
+
+function handleTownSelectChange(){
+ clearRegNumbers();
+  var newRegList= {};
+  var startsWith ;
+
+  var selecedTown = townSelectElem.value;
+
+  if (selecedTown == "cape town") {
+    startsWith = 'CA';
+  } else if (selecedTown == "paarl") {
+    startsWith = 'CJ';
+  } else if (selecedTown == "belville") {
+    startsWith = 'CY';
+  } else if (selecedTown == "strand") {
+    startsWith = 'CF';
+  }
+
+  regNos = addRegNum.getRegistrationNos();
+  Object.keys(regNos).map(regNo =>{
+   if (regNo.startsWith(startsWith)) {
+     newRegList[regNo] = 0;
+   }
+
   })
 
+  Object.keys(newRegList).map(regNo =>{
+    showRegNumber(regNo);
+  })
 }
