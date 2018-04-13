2e7339684a9beebdc801cbaa913ada405bbf1835
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-04-13 12:59:42 +0200

    remove duplicate 'return' text in test's 'it'

--- test/registration_numbers.tests.js
+++ test/registration_numbers.tests.js
@@ -10,37 +10,37 @@ describe('AddRegNum', function() {
       assert.deepEqual({"CA 123": 0, "CJ 345": 0, "CY 123": 0}, addRegNum.getRegistrationNos());
     });
 
-    it('should return return true if registration number starts with CA.', function() {
+    it('should return true if registration number starts with CA.', function() {
       var addRegNum = AddRegNum();
 
       assert.deepEqual(true, addRegNum.regNoFromTown('CA 123'));
     });
 
-    it('should return return true if registration number starts with CF.', function() {
+    it('should return true if registration number starts with CF.', function() {
       var addRegNum = AddRegNum();
 
       assert.deepEqual(true, addRegNum.regNoFromTown('CF 123'));
     });
 
-    it('should return return true if registration number starts with CJ.', function() {
+    it('should return true if registration number starts with CJ.', function() {
       var addRegNum = AddRegNum();
 
       assert.deepEqual(true, addRegNum.regNoFromTown('CJ 123'));
     });
 
-    it('should return return true if registration number starts with CY.', function() {
+    it('should return true if registration number starts with CY.', function() {
       var addRegNum = AddRegNum();
 
       assert.deepEqual(true, addRegNum.regNoFromTown('CY 123'));
     });
 
-    it('should return return false if registration number does not start with either CY, CA, CJ or CF.', function() {
+    it('should return false if registration number does not start with either CY, CA, CJ or CF.', function() {
       var addRegNum = AddRegNum();
 
       assert.deepEqual(false, addRegNum.regNoFromTown('CK 123'));
     });
 
-    it('should return return registration numbers from Cape Town.', function() {
+    it('should return registration numbers from Cape Town.', function() {
       var addRegNum = AddRegNum();
 
       addRegNum.addRegistrationNo('CA 123');
@@ -50,7 +50,7 @@ describe('AddRegNum', function() {
       assert.deepEqual({"CA 123": 0, "CA 987": 0}, addRegNum.filterByTown('cape town'));
     });
 
-    it('should return return registration numbers from Paarl.', function() {
+    it('should return registration numbers from Paarl.', function() {
       var addRegNum = AddRegNum();
 
       addRegNum.addRegistrationNo('CJ 123');
@@ -60,7 +60,7 @@ describe('AddRegNum', function() {
       assert.deepEqual({"CJ 123": 0, "CJ 987": 0}, addRegNum.filterByTown('paarl'));
     });
 
-    it('should return return registration numbers from Belville.', function() {
+    it('should return registration numbers from Belville.', function() {
       var addRegNum = AddRegNum();
 
       addRegNum.addRegistrationNo('CJ 123');
@@ -70,7 +70,7 @@ describe('AddRegNum', function() {
       assert.deepEqual({"CY 987": 0}, addRegNum.filterByTown('belville'));
     });
 
-    it('should return return registration numbers from Strand.', function() {
+    it('should return registration numbers from Strand.', function() {
       var addRegNum = AddRegNum();
 
       addRegNum.addRegistrationNo('CJ 123');
@@ -80,7 +80,7 @@ describe('AddRegNum', function() {
       assert.deepEqual({"CF 987": 0, 'CF 123': 0}, addRegNum.filterByTown('strand'));
     });
 
-    it('should return return all registration numbers when filter value is all.', function() {
+    it('should return all registration numbers when filter value is all.', function() {
       var addRegNum = AddRegNum();
 
       addRegNum.addRegistrationNo('CJ 123');
